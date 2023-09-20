cf98e52cb97c2eb14140c920b3a4fc919ba586eb
seansg <seansgarwood@gmail.com>
2023-09-20 17:26:03 -0400

    Add Google API, csv

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,12 +1,32 @@
 # frozen_string_literal = true
+
 require 'csv'
-puts "Event Manager Initialized!\n\n"
+require 'google/apis/civicinfo_v2'
 
-lines = File.readlines('event_attendees.csv')
-lines.each_with_index do |line, index|
-  next if index.zero?
+civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
 
-  columns = line.split(',')
-  name = columns[2]
-  puts name
+def clean_zipcode(zipcode)
+  zipcode.to_s.rjust(5, '0')[0..4]
+end
+
+puts "Event Manager initialized.\n\n"
+
+contents = CSV.open(
+  'event_attendees.csv',
+  headers: true,
+  header_converters: :symbol
+)
+
+contents.each do |row|
+  name = row[:first_name]
+  zipcode = clean_zipcode(row[:zipcode])
+  legislators = civic_info.representative_info_by_address(
+    address: zipcode,
+    levels: 'country',
+    roles: ['legislatorUpperBody', 'legislatorLowerBody']
+  )
+  legislators = legislators.officials
+
+  puts "#{name} #{zipcode} #{legislators}"
 end
